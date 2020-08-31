61b4b3863eaf353a8bc9a033f2522b5c2fab3511
PhoebeNjeri <Phoebegichuhi4@gmail.com>
2020-08-31 11:39:24 +0300

    added content to gitub.service.service.ts

--- gitproject/src/app/repos.service.spec.ts
+++ gitproject/src/app/repos.service.spec.ts
@@ -1,16 +1,8 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ReposService } from './repos.service';
 
 describe('ReposService', () => {
-  let service: ReposService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ReposService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should create an instance', () => {
+    expect(new ReposService()).toBeTruthy();
   });
 });
+  
