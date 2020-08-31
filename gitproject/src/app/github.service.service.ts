9dade2a1e3f0f1a022df5fa28472cf7f8cb6cca4
PhoebeNjeri <Phoebegichuhi4@gmail.com>
2020-08-31 10:55:43 +0300

    added content to github.service.service.ts

--- gitproject/src/app/github.service.service.ts
+++ gitproject/src/app/github.service.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable} from 'rxjs/Rx';
+import { repos} from './repos';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class Github.ServiceService {
+@Injectable()
 
-  constructor() { }
+export class GithubService {
+
+  baseURL: string = 'https://api.github.com/';
+  constructor(private http:HttpClient){
+  }
+
+  getRepos(userName:string): Observable<repos[]> {
+       return this.http.get<repos[]>(this.baseURL + 'users/' + userName + '/repos');
+  }
 }
